3571296cb97d187fed378454354a80d307146c56
Norulshahlam <norulshahlam@gmail.com>
2021-10-10 23:36:26 +0800

    d

--- product-service/src/main/java/shah/productservice/controllers/ProductRestController.java
+++ product-service/src/main/java/shah/productservice/controllers/ProductRestController.java
@@ -1,7 +1,11 @@
 package shah.productservice.controllers;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,17 +23,24 @@ public class ProductRestController {
 	@Autowired
 	private ProductRepo repo;
 
-	@Autowired
+	@Autowired 
 	private RestTemplate restTemplate;
 
 	@Value("${couponService.url}")
 	private String couponServiceURL;
 
 	@RequestMapping(value = "/products", method = RequestMethod.POST)
-	public Product create(@RequestBody Product product) {
+	public ResponseEntity<?> create(@RequestBody Product product) {
+
 		Coupon coupon = restTemplate.getForObject(couponServiceURL + product.getCouponCode(), Coupon.class);
+
+		if(Objects.isNull(coupon)){
+			product.setPrice(product.getPrice());
+			return new ResponseEntity<Object>(repo.save(product),HttpStatus.OK);
+		}
+
 		product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
-		return repo.save(product);
+		return new ResponseEntity<Object>(repo.save(product),HttpStatus.OK);
 
 	}
 }
