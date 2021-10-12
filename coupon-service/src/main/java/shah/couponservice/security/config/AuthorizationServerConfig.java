cf8548cf4390301e9f9da5fc4849046180924267
Norulshahlam <norulshahlam@gmail.com>
2021-10-12 12:40:15 +0800

    e

--- coupon-service/src/main/java/shah/couponservice/security/config/AuthorizationServerConfig.java
+++ coupon-service/src/main/java/shah/couponservice/security/config/AuthorizationServerConfig.java
@@ -1,6 +1,5 @@
 package shah.couponservice.security.config;
 
-
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,20 +24,21 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
   @Autowired
   private UserDetailsService userDetailsService;
-  
+
   @Autowired
   private PasswordEncoder passwordEncoder;
 
   @Autowired
   private DataSource dataSource;
 
-  // we are using database to store the credentials
+  // we are persisting the tokens generated in database
   @Override
   public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
     endpoints.tokenStore(new JdbcTokenStore(dataSource)).authenticationManager(authenticationManager)
         .userDetailsService(userDetailsService);
   }
-// we r using the hardcoded credentials
+
+  // we r using the hardcoded credentials
   @Override
   public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
     clients.inMemory().withClient("couponclientapp").secret(passwordEncoder.encode("9999"))
